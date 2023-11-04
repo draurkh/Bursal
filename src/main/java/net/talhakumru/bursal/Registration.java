8763e6bf848b48c5a859d2aecec0fc48c7692f8c
Talha Kumru <talha.kumru767@gmail.com>
2023-11-04 13:22:20 +0300

    Initial commit

---
+++ src/main/java/net/talhakumru/bursal/Registration.java
@@ -0,0 +1,59 @@
+package net.talhakumru.bursal;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Registration {
+	
+	private String firstName;
+	private String lastName;
+	private String birthday;
+	private String university;
+	private String address;
+	
+	public Registration() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
